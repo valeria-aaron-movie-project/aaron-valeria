21b95e4476965ca96a2ff3b321be3ef4cc9e3b07
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-05 14:54:49 -0500

    movie-nav-update

--- movie-project.js
+++ movie-project.js
@@ -22,11 +22,52 @@ async function buildMovies(){
                 <h3 class="title">
                     ${movie.title}
                 </h3>
-                <div class="movie-year">${movie.year}</div>
+                <div class="movie-year">Released: ${movie.year}</div>
             </div>`
         return movieCard;
     });
     $(".movie-layout").append(moviesHTML);
 }
 
+const moviesURL = "https://wide-past-waltz.glitch.me/movies";
+
+let modifyEvil = {
+    title: "Resident Evil"
+
+}
+let modifyDown = {
+    title: "Black Hawk Down"
+}
+
+let modifyTenet = {
+    title: "Tenet"
+}
+
+const patchEvil = {
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(modifyEvil)
+}
+
+const patchTenet = {
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(modifyTenet)
+}
+
+const patchDown = {
+    method: "PATCH",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(modifyDown)
+}
+
+fetch(moviesURL+ "/2", patchDown).then(getMovies);
+fetch(moviesURL+ "/3", patchTenet).then(getMovies);
+fetch(moviesURL+ "/4", patchEvil).then(getMovies);
 
