547e56fb79d41c85bacfe7bdca1af9021f0e0099
Valeria Canizales <vcanizales93@gmail.com>
2022-10-05 14:13:47 -0500

    movie project edit

--- movie-project.js
+++ movie-project.js
@@ -1,5 +1,7 @@
 buildMovies()
 
+
+//function to retrieve movies
 async function getMovies() {
     try {
         let response = await fetch(`https://wide-past-waltz.glitch.me/movies`);
@@ -11,22 +13,37 @@ async function getMovies() {
     }
 }
 
+//Function to build movies
 async function buildMovies(){
     let movies = await getMovies()
     console.log(movies);
     let moviesHTML = movies.map(function(movie, index) {
         let movieCard = `<div class="movie-card">
                 <div class="movie-pic-wrapper">
-                    <img class="movie-pic" src="${movie.poster}" />
+                    <img class="movie-pic" src="${movie.poster}"/>
                 </div>
                 <h3 class="title">
-                    ${movie.title}
+                    ${movie.title.toUpperCase()}
                 </h3>
-                <div class="movie-year">${movie.year}</div>
+                <div class="movie-year">Released in ${movie.year}</div>
             </div>`
         return movieCard;
     });
     $(".movie-layout").append(moviesHTML);
 }
+
+//Function to modify part of the entry
+let titleModification = {
+    title: ""
+}
+const patchOptions = {
+    method: 'PATCH',
+    title: {
+        'Content-Type' : 'applications/json'
+    },
+    body: JSON.stringify(titleModification)
+}
+
+fetch(getMovies + "/3", patchOptions).then(getMovies)
 
 
